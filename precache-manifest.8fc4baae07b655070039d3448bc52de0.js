1d8e9c228f0954bb7f605922311c065b8fb65e77
nzambello <nzambello@protonmail.com>
2020-07-02 12:48:44 +0200

    Updates

---
+++ precache-manifest.8fc4baae07b655070039d3448bc52de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c997cb44eaf3136a70660a9ecabca9",
+    "url": "/test-geocoding-react/index.html"
+  },
+  {
+    "revision": "83473d1a565e51163ad6",
+    "url": "/test-geocoding-react/static/css/main.ca8f9a50.chunk.css"
+  },
+  {
+    "revision": "4b3f4d3a691575755a9d",
+    "url": "/test-geocoding-react/static/js/2.c0138f6d.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/test-geocoding-react/static/js/2.c0138f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83473d1a565e51163ad6",
+    "url": "/test-geocoding-react/static/js/main.487712dd.chunk.js"
+  },
+  {
+    "revision": "fd72d72f199dd4096d0d",
+    "url": "/test-geocoding-react/static/js/runtime-main.2fd189bd.js"
+  }
+]);
